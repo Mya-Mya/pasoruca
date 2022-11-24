c5734c80404ac501ec285b21194f529c154e6dbd
Mya-Mya <39019907+Mya-Mya@users.noreply.github.com>
2022-11-24 10:32:24 +0900

    Create `load_config`

--- src/app/app.py
+++ src/app/app.py
@@ -1,5 +1,10 @@
 from typing import Dict
+import json
+from pathlib import Path
+import os
 from dataclasses import dataclass
+
+HOME_DIRECTORY = Path(os.path.expanduser("~"))
 
 
 @dataclass
@@ -10,3 +15,15 @@ class Config:
     absentText: str
 
 
+def load_config(fp: Path = HOME_DIRECTORY / "pasoruca.json") -> Config:
+    with open(fp, "r") as file:
+        data = json.load(file)
+    return Config(
+        roomid=str(data["roomId"]),
+        cardid_to_memberid={
+            str(cardid): str(memberid)
+            for cardid, memberid in data["cardidToMemberid"].items()
+        },
+        presentText=data["statuses"]["presentText"],
+        absentText=data["statuses"]["absentText"],
+    )
